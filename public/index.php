790b169f804cd53bb1beaa8e0318766e6c204b45
annacb21 <annacb21@gmail.com>
2020-11-26 17:48:37 +0100

    inseriti commenti in head

--- public/index.php
+++ public/index.php
@@ -9,8 +9,9 @@
         <meta name="author" content="Anna Cisotto Bertocco">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="Cache-control" content="no-cache" />
-        <title>Dott. Andrea Costacurta</title>
+        <title>Andrea Costacurta</title>
         <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <!-- Bootstrap stylesheet -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <!-- Bootstrap Scripts -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -20,7 +21,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
         <!-- Fontawesome icons -->
         <script src="https://kit.fontawesome.com/8284f330c3.js" crossorigin="anonymous"></script>
-        <!-- Animation library -->
+        <!-- Animation library Animate -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     </head>
     <body>
