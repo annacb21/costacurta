683dbf73be53224b9b721c355b4c2419599df85a
annacb21 <annacb21@gmail.com>
2020-11-30 12:45:20 +0100

    fatta pagina chi sono

--- resources/templates/front/chisono.php
+++ resources/templates/front/chisono.php
@@ -10,6 +10,7 @@
                 Ho cominciato a svolgere la mia professione come psicoterapeuta alla fine degli anni ottanta, per poi iscrivermi all’Albo degli Psicologi-Psicoterapeuti dal ’93 (anno in cui l’albo è stato costituito in Italia) con il numero 775 – essere una delle prime persone iscritte all’Albo mi è sempre stato di grande motivazione. 
                 In questa professione non esiste routine, ogni persona porta in sè un intero universo e nessuno è uguale all’altro – andare alla scoperta di questo universo, insieme al paziente, è quello che amo della mia professione.
             </p>
+            <button type="button" class="btn btn-outline-info btn-lg mt-5">Curriculum [pdf]</button>
         </div>
     </div>
 </div>
