fb5f823f02b118e818d87b4f25d098f6a79122d1
annacb21 <annacb21@gmail.com>
2020-12-11 09:39:09 +0100

    aggiunta connessione database

--- resources/config.php
+++ resources/config.php
@@ -13,18 +13,18 @@ defined("TEMPLATE_ADMIN") ? null : define("TEMPLATE_ADMIN", __DIR__ . DS . "temp
 
 //defined("UPLOADS") ? null : define("UPLOADS", __DIR__ . DS . "uploads");
 
-/*
+
 defined("DB_HOST") ? null : define("DB_HOST", "localhost");
 
 defined("DB_USER") ? null : define("DB_USER", "root");
 
 defined("DB_PASS") ? null : define("DB_PASS", "");
 
-defined("DB_NAME") ? null : define("DB_NAME", "studio_legale_db");
+defined("DB_NAME") ? null : define("DB_NAME", "costacurta_db");
 
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-*/
+
 
 require_once("functions.php");
 
