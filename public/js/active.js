e520373a3dcf486e014505b3050f1d5a22cf5359
annacb21 <annacb21@gmail.com>
2020-12-18 14:49:40 +0100

    aggiunto stile link dropdown attivo

--- public/js/active.js
+++ public/js/active.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
     var url = window.location;
-    $('ul.navbar-nav a[href="'+ url +'"]').addClass('active');
-    $('ul.navbar-nav a').filter(function() {
+    $('ul.navbar-nav a.nav-link[href="'+ url +'"]').addClass('active');
+    $('ul.navbar-nav a.dropdown-item[href="'+ url +'"]').addClass('active-drop');
+    $('ul.navbar-nav a.nav-link').filter(function() {
             return this.href == url;
-    }).addClass('active');
+    }).addClass('active');   
+    $('ul.navbar-nav a.dropdown-item').filter(function() {
+        return this.href == url;
+    }).addClass('active-drop'); 
 });
