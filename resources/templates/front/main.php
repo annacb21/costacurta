7ec9384b4e8865fd9fb73755e77134854bc1e75f
annacb21 <annacb21@gmail.com>
2020-12-23 12:14:48 +0100

    sistemata home page

--- resources/templates/front/main.php
+++ resources/templates/front/main.php
@@ -3,41 +3,10 @@
 
     <div class="col-lg-6">
         <h1 class="title">Chiedere aiuto <br> è il primo passo <br> per stare meglio.</h1>
-        <button type="button" class="btn btn-primary btn-lg mt-5">Prenota appuntamento</button>
+        <a role="button" href="../public/index.php?contatti" class="btn btn-primary btn-lg mt-5">Prenota appuntamento</a>
     </div>
     <div class="col-lg-6 animate__animated animate__fadeInLeft">
         <img src="../public/images/studio1.png" alt="foto studio Padova" class="mw-100 float-lg-right">
-    </div>
-
-</div>
-
-<!-- AREE DI INTERVENTO -->
-<div id="areas" class="my-5">
-
-    <h2 class="text-primary text-uppercase h4 mb-5">Aree di intervento</h2>
-
-    <div class="card-deck">
-        <div class="card">
-            <div class="card-header">Fobie</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <a href="#" class="btn btn-primary">Approfondisci</a>
-        </div>
-        <div class="card">
-            <div class="card-header">Fobie</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <a href="#" class="btn btn-primary">Approfondisci</a>
-        </div>
-        <div class="card">
-            <div class="card-header">Fobie</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <a href="#" class="btn btn-primary">Approfondisci</a>
-        </div>
     </div>
 
 </div>
@@ -50,15 +19,37 @@
             <img src="../public/images/profile.png" alt="foto Andrea Costacurta" class="w-75">
         </div>
         <div class="col-lg-7">
-            <h2 class="text-primary float-lg-right text-uppercase h4 pb-4">Lo psichiatra</h2>
-            <p class="float-lg-right subtitle">Dott. Andrea Costacurta</p>
-            <p class="float-lg-right text-justify">Tanti anni di esperienza nel campo della psicoterapia hanno continuato ad alimentare l’entusiasmo che provo per il mio lavoro. 
-                Il lavoro con le persone, le emozioni ed i sentimenti mi hanno da sempre dato grandi soddisfazioni.
-                Ho cominciato a svolgere la mia professione come psicoterapeuta alla fine degli anni ottanta, per poi iscrivermi all’Albo degli Psicologi-Psicoterapeuti dal ’93 (anno in cui l’albo è stato costituito in Italia) con il numero 775 – essere una delle prime persone iscritte all’Albo mi è sempre stato di grande motivazione. 
-                In questa professione non esiste routine, ogni persona porta in sè un intero universo e nessuno è uguale all’altro – andare alla scoperta di questo universo, insieme al paziente, è quello che amo della mia professione.
-            </p>
+            <div class="row">
+                <h2 class="text-primary float-lg-right text-uppercase h4 pb-4">Chi sono</h2>
+            </div>
+            <div class="row">
+                <p class="float-lg-right subtitle">Dott. Andrea Costacurta</p>
+            </div>
+            <div class="row">
+                <p class="float-lg-right text-justify">Sono Psichiatra, Psicoanalista e Dottore di Ricerca in Scienze Psichiatriche. 
+                Le competenze ad ampio raggio e la lunga esperienza clinica mi consentono di trattare sia i disturbi emotivi più comuni sia le patologie psichiche più gravi con progetti di cura personalizzati.
+                Mi occupo di formazione psicoanalitica e psicoterapeutica, conduco supervisioni e gruppi di intervisione.
+                Mi appassiona da sempre la relazione tra psichiatria, psicoanalisi e cinema.
+                </p>
+            </div>
+            <div class="row">
+                <a role="button" href="../public/index.php?chisono" class="btn btn-outline-primary btn-lg">Approfondisci</a>
+            </div>
         </div>
     </div>
+
+</div>
+
+<!-- AREE DI INTERVENTO -->
+<div id="areas" class="py-5">
+
+    <h2 class="text-primary text-uppercase h4 mb-5">Aree di intervento</h2>
+
+    <div class="row">
+        <?php get_area_thumbnails(); ?>
+    </div>
+
+    <a role="button" href="../public/index.php?aree" class="btn btn-outline-primary btn-lg">Approfondisci</a>
 
 </div>
 
