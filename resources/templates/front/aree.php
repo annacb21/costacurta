9c55d0df6dbbbe966b321730867541200a60c088
annacb21 <annacb21@gmail.com>
2020-12-27 16:44:38 +0100

    sistemato php aree

--- resources/templates/front/aree.php
+++ resources/templates/front/aree.php
@@ -1,26 +1,6 @@
 <div class="py-5">
     <h1 class="subtitle mb-5">Aree di intervento</h1>
-    <div class="card-deck">
-        <div class="card">
-            <div class="card-header">Fobie</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <a href="#" class="btn btn-primary">Approfondisci</a>
-        </div>
-        <div class="card">
-            <div class="card-header">Fobie</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <a href="#" class="btn btn-primary">Approfondisci</a>
-        </div>
-        <div class="card">
-            <div class="card-header">Fobie</div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <a href="#" class="btn btn-primary">Approfondisci</a>
-        </div>
+    <div class="row">
+        <?php get_area_thumbnails(); ?>
     </div>
 </div>
