b0ebc712583686685d128618213d5106be2574a1
annacb21 <annacb21@gmail.com>
2020-12-29 11:56:52 +0100

    sistemato css navbar

--- resources/templates/front/navbar.php
+++ resources/templates/front/navbar.php
@@ -1,67 +1,47 @@
 <nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container-fluid">
 
-        <!-- navbar brand or logo -->
-        <a class="navbar-brand" href="../public/index.php">
-            <div class="row">
-                <div class="col">
-                    <img src="../public/images/logo.svg" alt="logo" id="logo" class="d-inline-block align-top">
-                </div>
-                <div class="col">
-                    <p class="logo-title">Dott. Andrea Costacurta</p>
-                    <p class="lh-sm logo-desc">Medico Chirurgo <br/> Psichiatra - Psicoterapeuta - Psicoanalista</p>
-                </div>
+    <!-- navbar brand or logo -->
+    <a class="navbar-brand" href="../public/index.php">
+        <div class="row">
+            <div class="col">
+                <img src="../public/images/logo.svg" alt="logo" id="logo" class="d-inline-block align-top">
             </div>
-        </a>
-
-        <!-- hamburger menu button on smaller screens -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <!-- navbar items on the right -->
-        <div class="collapse navbar-collapse" id="navbarToggler">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a href="../public/index.php" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a href="../public/index.php?chisono" class="nav-link">Qualcosa su di me</a>
-                </li>
-                <li class="nav-item">
-                    <a href="../public/index.php?aree" class="nav-link">Aree di intervento</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" id="studioDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Lo studio</a>
-                    <ul class="dropdown-menu" aria-labelledby="studioDropdown">
-                        <?php
-                            for($i = 0; $i < count($studi); $i++) {
-                                echo "<li><a class='dropdown-item' href='../public/index.php?studio&id={$studi[$i]['studio_id']}'>{$studi[$i]['city']}</a></li>";
-                            }
-                        ?>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="../public/index.php?articoli" class="nav-link">Articoli</a>
-                </li>
-                <li class="nav-item">
-                    <a href="../public/index.php?contatti" class="nav-link">Contatti</a>
-                </li>
-                <li class="nav-item">
-                <?php
-
-                    if(isset($_SESSION['user'])) {
-                        echo "<a href='../public/admin/' class='nav-link'><i class='fas fa-user-cog'></i></a>";
-                    }
-                    else {
-                        echo "<a href='../public/index.php?login' class='nav-link'><i class='fas fa-user'></i></a>";
-                    }
-
-                ?>
-                </li>
-            </ul>
+            <div class="col">
+                <p class="logo-title">Dott. Andrea Costacurta</p>
+                <p class="lh-sm logo-desc">Medico Chirurgo <br/> Psichiatra - Psicoterapeuta - Psicoanalista</p>
+            </div>
         </div>
+    </a>
+
+    <!-- hamburger menu button on smaller screens -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- navbar items on the right -->
+    <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a href="../public/index.php" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="../public/index.php?chisono" class="nav-link">Qualcosa su di me</a>
+            </li>
+            <li class="nav-item">
+                <a href="../public/index.php?aree" class="nav-link">Aree di intervento</a>
+            </li>
+            <li class="nav-item">
+                <a href="../public/index.php?gallery" class="nav-link">Gallery</a>
+            </li>
+            <li class="nav-item">
+                <a href="../public/index.php?articoli" class="nav-link">News</a>
+            </li>
+            <li class="nav-item">
+                <a href="../public/index.php?contatti" class="nav-link">Contatti</a>
+            </li>
+        </ul>
     </div>
+
 </nav>
 
 <script src="../public/js/active.js"></script>
