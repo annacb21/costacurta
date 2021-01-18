70b17032381c16e878b2a8b2b2f112e17b1de991
annacb21 <annacb21@gmail.com>
2021-01-18 14:28:46 +0100

    modificati campi profilo

--- resources/templates/front/chisono.php
+++ resources/templates/front/chisono.php
@@ -5,13 +5,16 @@
     <div class="row py-5 px-5">
         <div class="col-lg-4 widget-pre">
             <div class="card profile shadow">
-                <img src="../resources/<?php echo display_image($profile['pro_foto']); ?>" class="card-img shadow" alt="...">
+                <img src="../resources/<?php echo display_image($profile['pro_foto']); ?>" class="card-img shadow" alt="foto Andrea Costacurta">
             </div>
             <a role="button" href="../resources/<?php echo display_image($profile['pro_cv']); ?>" class="btn home-btn btn-lg mt-5" target="_blank">Curriculum [pdf]</a>
         </div>
         <div class="col-lg-8 px-5">
             <h1 class="section text-uppercase pb-4 pl-2">Qualcosa su di me ...</h1>
             <p class="text-justify profile-desc"><?php echo html_entity_decode(htmlentities($profile['pro_desc'])); ?></p>
+            <p>ALTRI SERVIZI</p>
+            <p class="text-justify profile-desc"><?php echo html_entity_decode(htmlentities($profile['pro_servizi'])); ?></p>
+            <p>Psicoanalista formato con il Prof. Arnaldo Petterlini</p>
         </div>
     </div>
     <div class="widget-top mt-5 py-5 px-3">
