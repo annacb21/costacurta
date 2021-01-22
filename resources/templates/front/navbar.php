df9ab868d90d51cd216dfd9585f0adf2a00728e7
annacb21 <annacb21@gmail.com>
2021-01-22 12:48:23 +0100

    sistemato css navbar

--- resources/templates/front/navbar.php
+++ resources/templates/front/navbar.php
@@ -3,12 +3,12 @@
     <!-- navbar logo -->
     <a class="navbar-brand" href="../public/index.php">
         <div class="row">
-            <div class="col">
+            <div class="col pl-0">
                 <img src="../public/images/logo.svg" alt="logo" id="logo" class="d-inline-block align-top">
             </div>
-            <div class="col">
+            <div class="col pl-0">
                 <p class="logo-title">Dott. Andrea Costacurta</p>
-                <p class="lh-sm logo-desc">Medico Chirurgo <br/> Psichiatra - Psicoterapeuta - Psicoanalista</p>
+                <p class="logo-desc mb-0">Medico Chirurgo <br/> Psichiatra - Psicoterapeuta - Psicoanalista</p>
             </div>
         </div>
     </a>
@@ -22,22 +22,22 @@
     <div class="collapse navbar-collapse" id="navbarToggler">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a href="../public/index.php" class="nav-link">Home</a>
+                <a href="../public/index.php" class="nav-link px-4">Home</a>
             </li>
             <li class="nav-item">
-                <a href="../public/index.php?chisono" class="nav-link">Qualcosa su di me</a>
+                <a href="../public/index.php?chisono" class="nav-link px-4">Qualcosa su di me</a>
             </li>
             <li class="nav-item">
-                <a href="../public/index.php?aree" class="nav-link">Aree di intervento</a>
+                <a href="../public/index.php?aree" class="nav-link px-4">Aree di intervento</a>
             </li>
             <li class="nav-item">
-                <a href="../public/index.php?gallery" class="nav-link">Gallery</a>
+                <a href="../public/index.php?gallery" class="nav-link px-4">Gallery</a>
             </li>
             <li class="nav-item">
-                <a href="../public/index.php?articoli" class="nav-link">News</a>
+                <a href="../public/index.php?articoli" class="nav-link px-4">News</a>
             </li>
             <li class="nav-item">
-                <a href="../public/index.php?contatti" class="nav-link">Contatti</a>
+                <a href="../public/index.php?contatti" class="nav-link px-4">Contatti</a>
             </li>
         </ul>
     </div>
