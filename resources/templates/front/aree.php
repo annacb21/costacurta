a512d264abdbe80e358bdf4723206fa2c6b72163
annacb21 <annacb21@gmail.com>
2021-01-25 17:58:44 +0100

    fatto css pagina aree

--- resources/templates/front/aree.php
+++ resources/templates/front/aree.php
@@ -1,6 +1,55 @@
-<div class="page">
-    <h1 class="subtitle mb-5">Aree di intervento</h1>
-    <div class="row">
-        
+<!-- PAGE QUOTE -->
+<?php get_page_quote(2); ?>
+
+<!-- PAGE TITLE -->
+<div class="page-title mt-4 bg-color">
+    AREE DI INTERVENTO
+    <div class="w-25 borderBottomLight pt-2"></div>
+</div>
+
+<!-- AREAS --> 
+<div class="page piramide">
+    <div class="row justify-content-center align-items-center">
+        <div class="col-lg-6 pr-5 pt-4 area">
+            <div class="card area-card px-4 py-4">
+                <h5 class="area-title text-center pb-2">Psichiatria clinica</h5>
+                <p class="px-3 py-3">Diagnosi e cura, psicologica, medica e farmacologica dei principali disturbi quali:</p>
+                <ul>
+                    <li>depressione nell’adolescente, nell’adulto e nell’anziano</li>
+                    <li>disturbi d’ansia, attacchi di panico</li>
+                    <li>disturbo ossessivo compulsivo</li>
+                    <li>disturbi da stress e conseguenti a traumi</li>
+                    <li>disturbi psicosomatici ed ipocondriaci, somatizzazioni</li>
+                    <li>disturbi dell’alimentazione: obesità, anoressia e bulimia</li>
+                    <li>disturbi del sonno: insonnia, incubi e terrore notturno</li>
+                    <li>disturbi del controllo degli impulsi</li>
+                    <li>disturbi della memoria, demenze senili e demenza di Alzheimer</li>
+                    <li>psicosi schizofreniche e deliranti</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-lg-6 pl-5 pt-4 area">
+            <div class="card area-card px-4 py-4">
+                <h5 class="area-title text-center pb-2">Psicofarmacologia</h5>
+                <p class="px-3 py-3">Terapia medica dei disturbi psicologici e psichiatrici. Le terapie farmacologiche moderne sono sicure ed efficaci e se prescritte correttamente, prive di rischi. Comprendono terapie:</p>
+                <ul>
+                    <li>dell’ansia (ansiolitici) e di tutte le manifestazioni associate</li>
+                    <li>della depressione, anche grave (antidepressivi)</li>
+                    <li>dei disturbi del sonno (ipnoinducenti)</li>
+                    <li>dei più gravi disturbi psicotici (neurolettici e stabilizzatori dell’umore)</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-lg-6 pl-5 pt-2 area">
+            <div class="card area-card px-4 py-4">
+                <h5 class="area-title text-center pb-2">Psicoterapia</h5>
+                <ul class="py-3">
+                    <li>Psicoterapia della Crisi Emozionale</li>
+                    <li>Psicoterapia Interpersonale</li>
+                    <li>Psicoanalisi</li>
+                </ul>
+                <p class="px-3 py-3">A seconda del disturbo e delle personali esigenze può essere programmata una psicoterapia breve (20 sedute) oppure una psicoterapia ad indirizzo analitico (oltre 25 sedute) che agisce più in profondità ma con tempi più lunghi.</p>
+            </div>
+        </div>
     </div>
 </div>
